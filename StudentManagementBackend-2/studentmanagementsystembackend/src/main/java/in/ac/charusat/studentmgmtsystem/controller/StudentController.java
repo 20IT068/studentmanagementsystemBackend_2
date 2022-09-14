1ce648357a22de710e33bad70b17d04666eed240
20IT068 <111736553+20IT068@users.noreply.github.com>
2022-09-14 22:49:40 +0530

    Added Cross Origin

--- StudentManagementBackend-2/studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
+++ StudentManagementBackend-2/studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -1,5 +1,4 @@
 package in.ac.charusat.studentmgmtsystem.controller;
-
 import in.ac.charusat.studentmgmtsystem.model.Student;
 import in.ac.charusat.studentmgmtsystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,38 +7,34 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3004")
 public class StudentController {
 
     @Autowired
     StudentRepository studentRepository;
-
     // Get the list of all student
     @GetMapping("/listPatient")
     public List<Student> getAllStudents() 
     {
         return studentRepository.findAll();
     }
-
     // Get the student information
     @GetMapping("/patient/{id}")
     public Student getStudent(@PathVariable Integer id) {
         return studentRepository.findById(id).get();
     }
-
     // Delete the student
     @DeleteMapping("/patient/{id}")
     public List<Student> deleteStudent(@PathVariable Integer id) {
         studentRepository.delete(studentRepository.findById(id).get());
         return studentRepository.findAll();
     }
-
     // Add new student
     @PostMapping("/patient")
     public List<Student> addStudent(@RequestBody Student student) {
         studentRepository.save(student);
         return studentRepository.findAll();
     }
-
     // Update the student information
     @PutMapping("/patient/{id}")
     public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
@@ -49,5 +44,4 @@ public class StudentController {
         studentRepository.save(studentObj);
         return studentRepository.findAll();
     }
-
 }
