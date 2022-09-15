2226df7c4132e0c23d6858b352c5f5e6a533e5a1
20IT068 <111736553+20IT068@users.noreply.github.com>
2022-09-15 20:42:27 +0530

    Added security with custom users jdbc authentication using H2 database.

--- StudentManagementBackend-2/studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ StudentManagementBackend-2/studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package in.ac.charusat.studentmgmtsystem.config;
-
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,25 +10,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+        @Autowired
+        DataSource dataSource;
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-                auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
+
+                auth.jdbcAuthentication().dataSource(dataSource)
+                        .withDefaultSchema()
+                        .withUser("admin")
+                        .password("pass")
+                        .roles("ADMIN", "USER")
+                        .and()
+                        .withUser("user")
+                        .password("pass")
+                        .roles("USER");
+                // auth.inMemoryAuthentication()
+                // .withUser("admin")
+                // .password("admin")
+                // .roles("ADMIN");
         } 
 
-
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+        }
         // provide encoding without any opration this method automatic called by spring boot
         @Bean
         public PasswordEncoder getPasswordEncoder()
         {
                 return NoOpPasswordEncoder.getInstance();
         }
-
 }
